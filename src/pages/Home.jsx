e0b70798c1d68b41e4561ab219906633805d6ba1
JuanOt14 <juanfer1420@gmail.com>
2025-11-21 09:16:32 -0500

    fix visual elements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,23 +13,25 @@ import productsData from '../data/products.json';
 export function Home() {
     return (
         <div className='home-root'>
-            <section className='main' id='inicio'>
+            <section className='home-main' id='inicio'>
                 <p>¿Tienes un sueño?</p>
                 <p>Queremos ser el puente</p>
                 <p>entre tu sueño y la realidad</p>
                 <button className='btn-primary'>Unete a nuestra organización</button>
             </section>
-            <section className='msg'>
+            <section className='home-msg'>
                 <h3>Somos Gente que cuida a la gente, a toda la gente…</h3>
                 <p>Nosotros creemos que todos los sueños son importantes y queremos ser el puente para que los alcances</p>
                 <Heart strokeWidth={1} size={72} />
             </section>
-            <section className='working-with' >
+            <section className='home-working-with' >
                 <h3>Trabajamos con <b>OMNILIFE</b> y <b>SEYTÚ</b></h3>
                 <p>Aquí encontrarás productos que nutrirán tu cuerpo, detonarán la expresión de tu personalidad y te inspirarán a transformar tu entorno.</p>
             </section>
-            <section className='we-together' id='nosotros'>
-                <img src={logo} alt="" className='we-together-logo' />
+            <section className='home-we-together' id='nosotros'>
+                <div className='home-we-together-logo' >
+                    <img src={logo} alt="" />
+                </div>
                 <img src="" alt="ELIZABETH GARCIA y LILIANA GARCIA" />
                 <h4>Elizabeth García y Liliana García</h4>
                 <h5>DECÍDETE Y CONVIERTETE EN EMPRESARIO OMNILIFE</h5>
@@ -38,32 +40,32 @@ export function Home() {
                     Te ofrecemos el mejor asesoramiento sobre los productos <b>OMNILIFE</b> y la mejor guía para impulsar tu negocio, además de otros beneficios al formar parte de esta gran empresa.
                 </p>
             </section>
-            <section className='benefits' id='beneficios'>
+            <section className='home-benefits' id='beneficios'>
                 <h3>Únete y llega tan alto como tú quieras</h3>
                 <p>Incrementa tus oportunidades económicas con tu Negocio Independiente OMNILIFE.</p>
-                <div className='benefits-elements'>
-                    <article className='benefits-item'>
+                <div className='home-benefits-elements'>
+                    <article className='home-benefits-item'>
                         <Heart strokeWidth={1} size={60} />
                         <h4>Una oportunidad de <b>mejorar tu calidad de vida</b></h4>
                     </article>
-                    <article className='benefits-item'>
+                    <article className='home-benefits-item'>
                         <DollarSign strokeWidth={1} size={60} />
                         <h4>Obtener <b>nuevos ingresos</b></h4>
                     </article>
-                    <article className='benefits-item'>
+                    <article className='home-benefits-item'>
                         <Activity strokeWidth={1} size={60} />
                         <h4>Ayudar a gente a <b>mejorar su salud</b></h4>
                     </article>
-                    <article className='benefits-item'>
+                    <article className='home-benefits-item'>
                         <Star strokeWidth={1} size={60} />
                         <h4>Tu negocio te lleva a <b>cumplir tus sueños</b></h4>
                     </article>
                 </div>
                 <button className='btn-primary'>QUIERO UNIRME</button>
             </section>
-            <section className='products' id='productos'>
+            <section className='home-products' id='productos'>
                 <h3>Productos</h3>
-                <div className='products-container'>
+                <div className='home-products-container'>
                     {
                         productsData.map((item) => {
                             return (
@@ -75,7 +77,7 @@ export function Home() {
                 <h5>Cuída la salud y la de tu familia.</h5>
                 <p>OMNILIFE es la empresa de mayor crecimiento en el mundo de los suplementos nutricionales, OMNILIFE te ofrece la oportunidad de mejorar tu salud, ¿deseas comprar productos ahora?</p>
             </section>
-            <section className='join-us' id='unete'>
+            <section className='home-join-us' id='unete'>
                 <h3>Únete a nosotros</h3>
                 <p>Estás a un paso de convertirte en un exitoso empresario</p>
                 <button className='btn-primary'>Contactanos</button>
